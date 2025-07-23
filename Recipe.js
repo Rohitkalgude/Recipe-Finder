c5d5df72164a001ff4bd629ab911da44c50d47d7
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-07-23 16:14:14 +0530

    update

---
+++ Recipe.js
@@ -0,0 +1,10 @@
+const Searchinput = document.getElementById("search-input");
+const Searchbtn = document.getElementById("search-btn");
+const mealsContainer = document.getElementById("meals");
+const errorContainer = document.getElementById("error-container");
+const mealDetails = document.getElementById("meal-details");
+const resultHeading = document.getElementById("result-heading");
+const mealDetailsContent = document.getElementsByClassName(
+  "meal-details-content"
+);
+const backBtn = document.getElementById("back-btn");
